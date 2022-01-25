15a12a071f436739f43f24d2fe0141bb5af9a1a8
oculi-s <kjunhun9@sogang.ac.kr>
2022-01-25 23:12:11 +0900

    220125

---
+++ S2/BOJ_S2_C/15664_N과_M_(10).c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int n, m, a[8], i, j, t, p[10001], q[10001];
+
+void _r(int b[8], int c, int e) {
+	int i;
+	if (c == m) {
+		if (_c(b)) {
+			for (i = 0; i < m; i++)
+				printf("%d ", b[i]);
+			printf("\n");
+		}
+	}
+	else {
+		for (i = e; i < n; i++)
+			b[c] = a[i], _r(b, c + 1, i);
+	}
+}
+
+int _c(int b[8]) {
+	int c = 1;
+	for (i = 0; i < m; i++)
+		q[b[i]]++;
+	for (i = 1; i <= 10000; i++) {
+		if (p[i] < q[i])
+			c = 0;
+		q[i] = 0;
+	}
+	return c;
+}
+
+int main() {
+	scanf("%d %d", &n, &m);
+	for (i = 0; i < n; i++)
+		scanf("%d", &t), p[t]++;
+	n = 0;
+	for (i = 1; i <= 10000; i++)
+		if (p[i])
+			a[n++] = i;
+	int b[8] = { 0, };
+	_r(b, 0, 0);
+	return 0;
+}
