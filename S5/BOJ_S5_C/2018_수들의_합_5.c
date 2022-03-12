1b6bcebc566f8fac8ac0fac82255b4ded7734ce0
oculi-s <oculis0925@yonsei.ac.kr>
2022-03-12 15:49:24 +0900

    220312

--- S5/BOJ_S5_C/2018_수들의_합_5.c
+++ S5/BOJ_S5_C/2018_수들의_합_5.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
+int n, a, b, c, s;
 int main() {
-	int n, a = 1, b = 2, c = 1, s = 3;
-	scanf("%d", &n);
-	while (b <= n / 2 + 1) {
-		if (s == n)
-			c++, s -= a++;
-		else if (s > n)
-			s -= a++;
-		else if (s < n)
-			s += ++b;
-	}
-	printf("%d", c);
+    scanf("%d", &n);
+    b = 1, s = 1;
+    while (a <= n){
+        if (n > s)
+            b++, s += b;
+        else if (s > n)
+            s -= a, a++;
+        else
+            c++, a++, b++, s += (b-a+1);
+    }
+    printf("%d", c);
+    return 0;
 }
