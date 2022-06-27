c8fb164df0eb4f7a2c40be8e518fe858e2d96eeb
oculi-s <oculis0925@yonsei.ac.kr>
2022-06-27 22:51:47 +0900

    220627

---
+++ G2/BOJ_G2_C/12738_가장 긴 증가하는 부분 수열 3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define N 1000001
+#define M 1000000001
+
+int n, i, j, e = 1, a[N], b[N], d[N];
+
+int find(int v, int r) {
+	int l = 0, m;
+	while (l < r) {
+		m = (l + r) / 2;
+		if (b[m] < v)
+			l = m + 1;
+		else if (b[m] >= v)
+			r = m;
+	}
+	return r;
+}
+
+int main() {
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+		scanf("%d", a + i), a[i] += M;
+	for (i = 1; i <= n; i++) {
+		if (b[e - 1] < a[i])
+			b[e] = a[i], d[i] = e, e++;
+		else {
+			j = find(a[i], e);
+			d[i] = j;
+			if (a[i] < b[j])
+				b[j] = a[i];
+		}
+	}
+	printf("%d", e - 1);
+	return 0;
+}
